0eaa37dde56e74427b668c4c14c9a5694762b1c6
felipeps1 <felipeps1@hotmail.com>
2019-02-20 09:55:24 -0500

    Atualizando arquivo test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Build Branch Master - Felipe Pereira da Silva")
+        self.assertRegex(result.data.decode(), "Build Branch Master")
 
 
 if __name__ == "__main__":
